3b415ab2e15a8f19c449a6e0cf85571b4c14fc26
bactxph36951 <bactxph36951@fpt.edu.vn>
2024-11-07 16:55:54 +0700

    admin template

---
+++ Admin/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using Dtos.Accounts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(LoginRequest request)
+        {
+            return View();
+        }
+    }
+}
